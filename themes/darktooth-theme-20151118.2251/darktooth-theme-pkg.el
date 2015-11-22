467502c32ca1577c8f6c162677ad0df9aa4e0ac5
Kristina Spurgin <kristina@infomuse.net>
2015-11-21 22:58:09 -0500

    stop tracking themes

--- themes/darktooth-theme-20151118.2251/darktooth-theme-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "darktooth-theme" "20151118.2251" "Color theme for Emacs, when soothe and gruvbox collide" (quote nil))
--- themes/soft-charcoal-theme-20140420.943/soft-charcoal-theme-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "soft-charcoal-theme" "20140420.943" "Dark charcoal theme with soft colors" (quote nil))
