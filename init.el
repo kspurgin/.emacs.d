0628d507305acc2c869ca4a11cb07e1f90a564b7
Kristina Spurgin <kristina@infomuse.net>
2015-11-29 15:15:39 -0500

    Modularize cperl setup

--- init.el
+++ init.el
@@ -32,6 +32,11 @@
 (setq load-prefer-newer t)
 (message "use-package is set up now")
 
+(use-package cperl-mode
+  :mode "\\.p[lm]\\'"
+  :interpreter "perl"
+  :config (load "cperl-setup"))
+
 ;; stuff for Cygwin
 (cond ((string-equal system-name "AALTS02.lib.unc.edu")
        (require 'windows-path)
@@ -64,6 +69,7 @@
        (load-theme 'darktooth t)
        (message "loaded theme")
        ))
+
 
 
 ;; -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
@@ -242,76 +248,6 @@
 ; From http://doc.norang.ca/org-mode.html
 
 (setq org-id-link-to-org-use-id 'create-if-interactive-and-no-custom-id)
-;############################################################################
-;#   Emacs config (Recommended) from Appendix C of "Perl Best Practices"    #
-;#     Copyright (c) O'Reilly & Associates, 2005. All Rights Reserved.      #
-;#  See: http://www.oreilly.com/pub/a/oreilly/ask_tim/2001/codepolicy.html  #
-;############################################################################
-; Added 20130612
-
-;; Use cperl mode instead of the default perl mode
-(defalias 'perl-mode 'cperl-mode)
-
-;; turn autoindenting on
-(global-set-key "\r" 'newline-and-indent)
-
-;; Insert spaces instead of tabs
-(add-hook 'cperl-mode-hook
-          '(lambda ()
-             (setq indent-tabs-mode nil)
-	     (setq cperl-tab-always-indent t)
-	     (setq cperl-indent-parens-as-block t)
-	     (setq cperl-indent-level 4)
-	     (setq cperl-continued-statement-offset 4)
-	     (setq cperl-close-paren-offset -4)
-	     (add-to-list 'write-file-functions 'delete-trailing-whitespace)))
-
-;; Set line width to 78 columns...
-(setq fill-column 78)
-(setq auto-fill-mode t)
-
-;; Use % to match various kinds of brackets...
-;; See: http://www.lifl.fr/~hodique/uploads/Perso/patches.el
-(global-set-key "%" 'match-paren)
-(defun match-paren (arg)
-  "Go to the matching paren if on a paren; otherwise insert %."
-  (interactive "p")
-  (let ((prev-char (char-to-string (preceding-char)))
-        (next-char (char-to-string (following-char))))
-    (cond ((string-match "[[{(<]" next-char) (forward-sexp 1))
-          ((string-match "[\]})>]" prev-char) (backward-sexp 1))
-          (t (self-insert-command (or arg 1))))))
-
-;; Load an applicationtemplate in a new unattached buffer...
-(defun application-template-pm ()
-  "Inserts the standard Perl application template"  ; For help and info.
-  (interactive "*")                                 ; Make this user accessible.
-  (switch-to-buffer "application-template-pm")
-  (insert-file "~/.code_templates/perl_application.pl"))
-;; Set to a specific key combination...
-;;(global-set-key "\C-ca" 'application-template-pm)
-
-;; Load a module template in a new unattached buffer...
-(defun module-template-pm ()
-  "Inserts the standard Perl module template"       ; For help and info.
-  (interactive "*")                                 ; Make this user accessible.
-  (switch-to-buffer "module-template-pm")
-  (insert-file "~/.code_templates/perl_module.pl"))
-;; Set to a specific key combination...
-(global-set-key "\C-cm" 'module-template-pm)
-
-;; Expand the following abbreviations while typing in text files...
-(abbrev-mode 1)
-
-(define-abbrev-table 'global-abbrev-table '(
-    ("pdbg"   "use Data::Dumper qw( Dumper );\nwarn Dumper[];"   nil 1)
-    ("phbp"   "#! /usr/bin/perl -w"                              nil 1)
-    ("pbmk"   "use Benchmark qw( cmpthese );\ncmpthese -10, {};" nil 1)
-    ("pusc"   "use Smart::Comments;\n\n### "                     nil 1)
-    ("putm"   "use Test::More 'no_plan';"                        nil 1)
-    ))
-
-(add-hook 'text-mode-hook (lambda () (abbrev-mode 1)))
 
 ;; -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 ;; copy full path of buffer
