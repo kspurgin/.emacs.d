d23e5c9f458507b504fa7a6c5a159b736eb01cf7
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2021-07-09 15:40:16 -0400

    use ibuffer-vc

--- init.el
+++ init.el
@@ -309,6 +309,27 @@
 :bind ("C-x C-b" . ibuffer)
 )
 
+(use-package ibuffer-vc
+ :ensure t
+ :config
+   (add-hook 'ibuffer-hook
+     (lambda ()
+       (ibuffer-vc-set-filter-groups-by-vc-root)
+       (unless (eq ibuffer-sorting-mode 'alphabetic)
+         (ibuffer-do-sort-by-alphabetic))))
+   (setq ibuffer-formats
+         '((mark modified read-only vc-status-mini " "
+                 (name 18 18 :left :elide)
+                 " "
+                 (size 9 -1 :right)
+                 " "
+                 (mode 16 16 :left :elide)
+                 " "
+                 (vc-status 16 16 :left)
+                 " "
+                 vc-relative-file)))
+)
+
 (setq ibuffer-saved-filter-groups
     '(("filters"
       ("magit" (name .".*magit"))
