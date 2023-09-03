3180822c4df4de676098fee4ad5ecde11daba871
Kristina Spurgin <kristina@infomuse.net>
2023-09-03 17:38:23 -0400

    Move autocustomization config to its own file

---
+++ auto-customize.el
@@ -0,0 +1,8 @@
+  (custom-set-variables
+   ;; custom-set-variables was added by Custom.
+   ;; If you edit it by hand, you could mess it up, so be careful.
+   ;; Your init file should contain only one such instance.
+   ;; If there is more than one, they won't work right.
+   '(package-selected-packages
+     (quote
+      (yaml-mode nxml-mode enh-ruby-mode yafolding adoc-mode php-mode yasnippet visual-regexp-steroids use-package move-text markdown-mode darktooth-theme auto-org-md auto-compile))))
