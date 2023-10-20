bbef2f5cf6cc0a4fda9a2313ee8409817b05e6e2
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2023-10-20 16:37:04 -0400

    Set autocustomize separate file

--- auto-customize.el
+++ auto-customize.el
@@ -1,8 +1,20 @@
-  (custom-set-variables
-   ;; custom-set-variables was added by Custom.
-   ;; If you edit it by hand, you could mess it up, so be careful.
-   ;; Your init file should contain only one such instance.
-   ;; If there is more than one, they won't work right.
-   '(package-selected-packages
-     (quote
-      (yaml-mode nxml-mode enh-ruby-mode yafolding adoc-mode php-mode yasnippet visual-regexp-steroids use-package move-text markdown-mode darktooth-theme auto-org-md auto-compile))))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-agenda-files
+   '("/Users/kristina/org/mig/wpl_westerville_public_library_cs.org" "/Users/kristina/org/mig/lafayette_college_collectionspace.org" "/Users/kristina/org/mig/az_ccp_cs.org" "/Users/kristina/org/mig/omca_authority_swap.org" "/Users/kristina/org/cspace.org" "/Users/kristina/org/islandora.org" "/Users/kristina/org/meetings.org" "/Users/kristina/org/migrations.org" "/Users/kristina/org/work.org"))
+ '(package-selected-packages
+   '(yaml-mode nxml-mode enh-ruby-mode yafolding adoc-mode php-mode yasnippet visual-regexp-steroids use-package move-text markdown-mode darktooth-theme auto-org-md auto-compile)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(adoc-meta-face ((t (:foreground "pink1" :height 100))))
+ '(adoc-meta-hide-face ((t (:foreground "gray40" :height 100))))
+ '(adoc-reference-face ((t (:foreground "pink1"))))
+ '(button ((t (:background "#2e3440" :foreground "#88c0d0" :box (:line-width (2 . 2) :color "#555555" :style sunken-button)))))
+ '(org-headline-done ((t (:foreground "gray50")))))
