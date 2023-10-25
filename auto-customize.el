ad401797fd8deec28fed9b7e3420c58e0aa7b5e1
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2023-10-25 16:20:10 -0400

    Move redundant stuff from auto config

--- auto-customize.el
+++ auto-customize.el
@@ -1,18 +1,8 @@
-
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(yaml-mode nxml-mode enh-ruby-mode yafolding adoc-mode php-mode yasnippet visual-regexp-steroids use-package move-text markdown-mode darktooth-theme auto-org-md auto-compile)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(adoc-meta-face ((t (:foreground "pink1" :height 100))))
- '(adoc-meta-hide-face ((t (:foreground "gray40" :height 100))))
- '(adoc-reference-face ((t (:foreground "pink1"))))
- '(button ((t (:background "#2e3440" :foreground "#88c0d0" :box (:line-width (2 . 2) :color "#555555" :style sunken-button)))))
- '(org-headline-done ((t (:foreground "gray50")))))
+ ;;'(package-selected-packages
+ ;;  '(yaml-mode nxml-mode enh-ruby-mode yafolding adoc-mode php-mode yasnippet visual-regexp-steroids use-package move-text markdown-mode darktooth-theme auto-org-md auto-compile))
+)
